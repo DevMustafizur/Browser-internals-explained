4c972d159550d8fc613aceae9054edaf1a37e4f7
Mustafizur <mustafizurrahman1256@gmail.com>
2026-06-02 13:07:08 +0600

    add DOM selector API notes

---
+++ web-api/dom-api/selector-api/script.js
@@ -0,0 +1,7 @@
+const aboutSection = document.getElementById('about')
+const listItems = document.getElementsByClassName("list")
+const lists = document.getElementsByTagName("li")
+const aboutSectinoByQuery = document.querySelector("#about")
+const containerByQuery = document.querySelector(".container")
+const listByQuery = document.querySelectorAll("li")
+const listItemByQuery = document.querySelectorAll(".list")
